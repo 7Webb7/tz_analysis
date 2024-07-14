96a4a95d13a5c94d57396dad6258fe1c004cb683
7Webb7 <v.dovybortsev@gmail.com>
2024-07-14 20:34:41 +0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-q
+import pandas as pd
+
+data_branches = pd.read_csv("data_files/t_branches.csv")
+df_branches = pd.DataFrame(data_branches)
+
+data_sales = pd.read_csv("data_files/t_sales.csv")
+df_sales = pd.DataFrame(data_sales)
+
+df_branches.fillna('', inplace=True)
+
+
+def determine_building_type(first, second):
+    if 'склад' in first.lower() or 'склад' in second.lower():
+        return "Склад"
+    else:
+        return "Магазин"
+
+
+df_branches['Тип'] = df_branches.apply(
+    lambda row: determine_building_type(row['Наименование'], row['КраткоеНаименование']), axis=1)
+df_branches.rename(columns={"Ссылка": "Филиал"}, inplace=True)
+df_merged = pd.merge(df_branches, df_sales, on="Филиал")
+
+df_by_sales = df_merged.groupby(["Филиал", "Наименование", "Тип"])["Продажа"].size().reset_index()
+
+df_by_sales_shops = df_by_sales[df_by_sales["Тип"] == "Магазин"]
+
+df_top_by_sales_shops = df_by_sales_shops.sort_values(by="Продажа", ascending=False).head(10).reset_index()
+df_top_by_sales_shops.drop(columns=["index"], inplace=True)
+print(df_top_by_sales_shops) #Топ 10 магазинов по продажам
+
+df_by_sales_warehouses = df_by_sales[df_by_sales["Тип"] == "Склад"]
+df_top_by_sales_warehouses = df_by_sales_warehouses(by="Продажа", ascending=False).head(10).reset_index()
+df_top_by_sales_shops.drop(columns=["index"], inplace=True)
+print(df_top_by_sales_warehouses)
