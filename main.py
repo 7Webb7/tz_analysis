273a2a88d07611ab1a95b1f66fa31aa35fb97b20
7Webb7 <v.dovybortsev@gmail.com>
2024-07-18 01:32:34 +0500

    1

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import re
-data_branches = pd.read_csv("data_files/t_branches.csv")
-df_branches = pd.DataFrame(data_branches)
+df_sales = pd.read_csv("data_files/t_sales.csv")
+sales = pd.DataFrame(df_sales)
 
-data_sales = pd.read_csv("data_files/t_sales.csv")
-df_sales = pd.DataFrame(data_sales)
+df_branches = pd.read_csv("data_files/t_branches.csv")
+branches = pd.DataFrame(df_branches)
 
+df_products = pd.read_csv("data_files/t_products.csv")
+products = pd.DataFrame(df_products)
+
+sales.rename(columns = {"Unnamed: 0" : "Index", "Филиал" : "Ссылка"}, inplace = True)
+#sales.set_index("Index", inplace = True)
+
+branches.rename(columns={"Unnamed: 0": "Index"}, inplace=True)
 df_branches.fillna('', inplace=True)
 
 
@@ -18,20 +25,14 @@ def determine_building_type(first, second):
         return "Магазин"
 
 
-df_branches['Тип'] = df_branches.apply(
-    lambda row: determine_building_type(row['Наименование'], row['КраткоеНаименование']), axis=1)
-df_branches.rename(columns={"Ссылка": "Филиал"}, inplace=True)
-df_merged = pd.merge(df_branches, df_sales, on="Филиал")
+branches['Тип'] = branches.apply(lambda row: determine_building_type(row['Наименование'], row['КраткоеНаименование']),
+                                 axis=1)
+merged_b_s = pd.merge(sales, branches, on = "Ссылка")
 
-df_by_sales = df_merged.groupby(["Филиал", "Наименование", "Тип"])["Продажа"].size().reset_index()
-
-df_by_sales_shops = df_by_sales[df_by_sales["Тип"] == "Магазин"]
-
-df_top_by_sales_shops = df_by_sales_shops.sort_values(by="Продажа", ascending=False).head(10).reset_index()
-df_top_by_sales_shops.drop(columns=["index"], inplace=True)
-print(df_top_by_sales_shops) #Топ 10 магазинов по продажам
-
-df_by_sales_warehouses = df_by_sales[df_by_sales["Тип"] == "Склад"]
-df_top_by_sales_warehouses = df_by_sales_warehouses(by="Продажа", ascending=False).head(10).reset_index()
-df_top_by_sales_shops.drop(columns=["index"], inplace=True)
-print(df_top_by_sales_warehouses)
+sales_number_types = merged_b_s.groupby(["Тип", "Ссылка", "Наименование"])["Количество"].sum().reset_index()
+warehouses_sales = sales_number_types[sales_number_types["Тип"] == "Склад"]
+shops_sales = sales_number_types[sales_number_types["Тип"] == "Магазин"]
+shops_sales_ten = shops_sales.sort_values(by = "Количество", ascending = False).head(10).reset_index()
+warehouses_sales_ten = warehouses_sales.sort_values(by = "Количество", ascending = False).head(10).reset_index()
+print(shops_sales_ten)
+print(warehouses_sales_ten)
